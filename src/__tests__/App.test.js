84804bc6348e24a6079943814b9da4a3e39147a1
riguel figueiro <riguelfigueiro@riguels-MacBook-Pro.local>
2020-01-19 15:49:49 -0300

    added test configurations

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Main/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('should make an test', () => {
+  expect(1 + 1).toBe(2);
+})
